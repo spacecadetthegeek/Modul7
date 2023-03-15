03af4e2701fa1100319eb570426ca8b3cfc617a2
spacecadetthegeek <mrvovashka@gmail.com>
2023-03-15 23:28:27 +0300

    Добавьте файлы проекта.

---
+++ Modul7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Employee
+{
+    public string Name;
+    public int Age;
+    public int Salary;
+}
+
+public class ProjectManager : Employee
+{
+    public string ProjectName;
+}
+
+public class Developer : Employee
+{
+    public string ProgrammingLanguage;
+}
